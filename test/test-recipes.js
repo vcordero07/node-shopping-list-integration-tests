bef4570b5fe6ab069a7060ab1868ec067b2cd08c
vcordero07 <vcordero07@gmail.com>
2017-08-15 23:18:15 -0400

    wip-added test for get endpoint"

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -7,7 +7,7 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('recipes', function() {
+describe('Recipes', function() {
   before(function() {
     return runServer();
   });
@@ -17,7 +17,21 @@ describe('recipes', function() {
   });
 
   it('should list items on GET', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
 
+      res.body.should.have.length.of.at.least(1);
+
+      const expectedKeys = ['id', 'name', 'ingredients'];
+      res.body.forEach(function(item) {
+        item.should.be.a('object');
+        item.should.include.keys(expectedKeys);
+      });
+    });
   });
 
   it('should add an item on POST', function() {
